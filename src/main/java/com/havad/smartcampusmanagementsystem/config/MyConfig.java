34590aa973675ab3f9e7a8a17a1714afe453d013
wzzzzx <wzx1360664415@outlook.com>
2023-04-22 21:31:14 +0800

    完成Mybatis分类插件配置 MyConfig.java

---
+++ src/main/java/com/havad/smartcampusmanagementsystem/config/MyConfig.java
@@ -0,0 +1,24 @@
+package com.havad.smartcampusmanagementsystem.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: SmartCampusManagementSystem
+ * @description: Mybatis分页插件配置
+ * @author: Havad
+ * @create: 2023-04-22 21:23
+ **/
+@Configuration
+@MapperScan("com.havad.smartcampusmanagementsystem.mapper")
+public class MyConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置最大单页限制数量（默认500)
+        paginationInterceptor.setLimit(200);
+        return paginationInterceptor;
+    }
+}
